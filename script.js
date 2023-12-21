cbcc036c784dc45e61f14822114ec9c6a6363b4c
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-21 12:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let n = shortHex.length - 3;
+	let output = "#";
+	let iscapital = false;
+	while(n < shortHex.length){
+	let ascii = shortHex.charCodeAt(0);
+	if(ascii >= 65 && ascii <=90){
+		iscapital = true;
+	}
+	output += shortHex[n] + shortHex[n];
+	n++;
+	}
+	return iscapital ? output.toUpperCase() : output;
 };
 
 // Do not change the code below.
